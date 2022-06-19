216643915d2cd3fd58d011630dc77dd3bb6fe8f5
gaurav kulwal <47554222+gauravkulwal@users.noreply.github.com>
2022-06-19 21:23:45 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { CreateComponent } from './contact/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo: 'list' , pathMatch: 'full'},
+  {path: 'list' , component:  ContactComponent },
+  {path: 'create' , component:  CreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './contact/contact.component';
+import { CreateComponent } from './contact/create/create.component';
+import {  FormsModule,  ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { ContactService } from './shared/contact.service';
+import {MatTableModule} from '@angular/material/table';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+     ReactiveFormsModule,
+     AngularFireModule.initializeApp(environment.firebaseConfig),
+     AngularFireDatabaseModule,
+     MatIconModule,
+     MatInputModule,
+     FormsModule,
+     MatTableModule,
+     MatFormFieldModule,
+     MatExpansionModule,
+     Ng2SearchPipeModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
